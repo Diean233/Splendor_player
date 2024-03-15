ced07cd935ca2f2facd568e7f8aa7cc80f949116
Diean233 <wdxiop@outlook.com>
2024-03-14 21:32:06 -0700

    playing

---
+++ play.py
@@ -0,0 +1,146 @@
+from .formulation import Card, Board, Player, Gem, Noble
+import random
+
+# total amount of noble cards
+noble_amount = 10
+# total amount of gem cards
+card_amount = 90
+
+# standard cards from the game
+# https://docs.google.com/spreadsheets/d/15ghp8rJ_vdVgxZIVJGawAYQXRMZSVHJYpZRfQUplAhE/htmlview
+CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
+         Card([1, 2, 1, 1, 0], 0, Gem.BLACK),
+         Card([2, 2, 0, 1, 0], 0, Gem.BLACK),
+         Card([0, 0, 1, 3, 1], 0, Gem.BLACK),
+         Card([0, 0, 2, 1, 0], 0, Gem.BLACK),
+         Card([2, 0, 2, 0, 0], 0, Gem.BLACK),
+         Card([0, 0, 3, 0, 0], 0, Gem.BLACK),
+         Card([0, 4, 0, 0, 0], 1, Gem.BLACK),
+
+         Card([1, 0, 1, 1, 1], 0, Gem.BLUE),
+         Card([1, 0, 1, 2, 1], 0, Gem.BLUE),
+         Card([1, 0, 2, 2, 0], 0, Gem.BLUE),
+         Card([0, 1, 3, 1, 0], 0, Gem.BLUE),
+         Card([1, 0, 0, 0, 2], 0, Gem.BLUE),
+         Card([0, 0, 2, 0, 2], 0, Gem.BLUE),
+         Card([0, 0, 0, 0, 3], 0, Gem.BLUE),
+         Card([0, 0, 0, 4, 0], 1, Gem.BLUE),
+
+         Card([0, 1, 1, 1, 1], 0, Gem.WHITE),
+         Card([0, 1, 2, 1, 1], 0, Gem.WHITE),
+         Card([0, 2, 2, 0, 1], 0, Gem.WHITE),
+         Card([3, 1, 0, 0, 1], 0, Gem.WHITE),
+         Card([0, 0, 0, 2, 1], 0, Gem.WHITE),
+         Card([0, 2, 0, 0, 2], 0, Gem.WHITE),
+         Card([0, 3, 0, 0, 0], 0, Gem.WHITE),
+         Card([0, 0, 4, 0, 0], 1, Gem.WHITE),
+         
+         Card([1, 1, 0, 1, 1], 0, Gem.GREEN),
+         Card([1, 1, 0, 1, 2], 0, Gem.GREEN),
+         Card([0, 1, 0, 2, 2], 0, Gem.GREEN),
+         Card([1, 3, 1, 0, 0], 0, Gem.GREEN),
+         Card([2, 1, 0, 0, 0], 0, Gem.GREEN),
+         Card([0, 2, 0, 2, 0], 0, Gem.GREEN),
+         Card([0, 0, 0, 3, 0], 0, Gem.GREEN),
+         Card([0, 0, 0, 0, 4], 1, Gem.GREEN),
+         
+         Card([1, 1, 1, 0, 1], 0, Gem.RED),
+         Card([2, 1, 1, 0, 1], 0, Gem.RED),
+         Card([2, 0, 1, 0, 2], 0, Gem.RED),
+         Card([1, 0, 0, 1, 3], 0, Gem.RED),
+         Card([0, 2, 1, 0, 0], 0, Gem.RED),
+         Card([2, 0, 0, 2, 0], 0, Gem.RED),
+         Card([3, 0, 0, 0, 0], 0, Gem.RED),
+         Card([4, 0, 0, 0, 0], 1, Gem.RED),
+         
+         Card([3, 2, 2, 0, 0], 1, Gem.BLACK),
+         Card([3, 0, 3, 0, 2], 1, Gem.BLACK),
+         Card([0, 1, 4, 2, 0], 2, Gem.BLACK),
+         Card([0, 0, 5, 3, 0], 2, Gem.BLACK),
+         Card([5, 0, 0, 0, 0], 2, Gem.BLACK),
+         Card([0, 0, 0, 0, 6], 3, Gem.BLACK),
+         
+         Card([0, 2, 2, 3, 0], 1, Gem.BLUE),
+         Card([0, 2, 3, 0, 3], 1, Gem.BLUE),
+         Card([5, 3, 0, 0, 0], 2, Gem.BLUE),
+         Card([2, 0, 0, 1, 4], 2, Gem.BLUE),
+         Card([0, 5, 0, 0, 0], 2, Gem.BLUE),
+         Card([0, 6, 0, 0, 0], 3, Gem.BLUE),
+
+         Card([0, 0, 3, 2, 2], 1, Gem.WHITE),
+         Card([2, 3, 0, 3, 0], 1, Gem.WHITE),
+         Card([0, 0, 1, 4, 2], 2, Gem.WHITE),
+         Card([0, 0, 0, 5, 3], 2, Gem.WHITE),
+         Card([0, 0, 0, 5, 0], 2, Gem.WHITE),
+         Card([6, 0, 0, 0, 0], 3, Gem.WHITE),
+
+         Card([3, 0, 2, 3, 0], 1, Gem.GREEN),
+         Card([2, 3, 0, 0, 2], 1, Gem.GREEN),
+         Card([4, 2, 0, 0, 1], 2, Gem.GREEN),
+         Card([0, 5, 3, 0, 0], 2, Gem.GREEN),
+         Card([0, 0, 5, 0, 0], 2, Gem.GREEN),
+         Card([0, 0, 6, 0, 0], 3, Gem.GREEN),
+
+         Card([2, 0, 0, 2, 3], 1, Gem.RED),
+         Card([0, 3, 0, 2, 3], 1, Gem.RED),
+         Card([1, 4, 2, 0, 0], 2, Gem.RED),
+         Card([3, 0, 0, 0, 5], 2, Gem.RED),
+         Card([0, 0, 0, 0, 5], 2, Gem.RED),
+         Card([0, 0, 0, 6, 0], 3, Gem.RED),
+         
+         Card([3, 3, 5, 3, 0], 3, Gem.BLACK),
+         Card([0, 0, 0, 7, 0], 4, Gem.BLACK),
+         Card([0, 0, 3, 6, 3], 4, Gem.BLACK),
+         Card([0, 0, 0, 7, 3], 5, Gem.BLACK),
+
+         Card([3, 0, 3, 3, 5], 3, Gem.BLUE),
+         Card([7, 0, 0, 0, 0], 4, Gem.BLUE),
+         Card([6, 3, 0, 0, 3], 4, Gem.BLUE),
+         Card([7, 3, 0, 0, 0], 5, Gem.BLUE),
+
+         Card([0, 3, 3, 5, 3], 3, Gem.WHITE),
+         Card([0, 0, 0, 0, 7], 4, Gem.WHITE),
+         Card([3, 0, 0, 3, 6], 4, Gem.WHITE),
+         Card([3, 0, 0, 0, 7], 5, Gem.WHITE),
+
+         Card([5, 3, 0, 3, 3], 3, Gem.GREEN),
+         Card([0, 7, 0, 0, 0], 4, Gem.GREEN),
+         Card([3, 6, 3, 0, 0], 4, Gem.GREEN),
+         Card([0, 7, 3, 0, 0], 5, Gem.GREEN),
+
+         Card([3, 5, 3, 0, 3], 3, Gem.RED),
+         Card([0, 0, 7, 0, 0], 4, Gem.RED),
+         Card([0, 3, 6, 3, 0], 4, Gem.RED),
+         Card([0, 0, 7, 3, 0], 5, Gem.RED),]
+
+NOBLES = [(0, 0, 0, 4, 4),
+          (3, 0, 3, 0, 3),
+          (0, 4, 4, 0, 0),
+          (4, 0, 4, 0, 0),
+          (0, 4, 0, 4, 0),
+          (0, 3, 0, 3, 3),
+          (0, 3, 3, 3, 0),
+          (4, 4, 0, 0, 0),
+          (3, 3, 3, 0, 0),
+          (3, 0, 0, 3, 3),]
+
+def greedy_play(p:Player, b:Board, p:int, s:int=0):
+    pass
+
+def play(n:int=card_amount, m:int=noble_amount, p:int=15, T:int=5):
+    # initialization
+    cards = CARDS
+    if n < card_amount:
+        cards = random.sample(CARDS, n)
+    
+    nobles = NOBLES
+    if m < noble_amount:
+        nobles = random.sample(NOBLES, m)
+    
+    p = Player()
+
+    b = Board(cards, nobles, T)
+
+    # play with greedy algo
+    return greedy_play(p, b, p)
+    pass
