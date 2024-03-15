367934610ffcd8d3f3ef43e28ad0e18fe233eae7
Olin <1961084629@qq.com>
2024-03-14 22:01:24 -0700

    simple method

--- play.py
+++ play.py
@@ -1,4 +1,5 @@
-from .formulation import Card, Board, Player, Gem, Noble
+from Player import Player
+from formulation import Card, Board, Gem, Noble
 import random
 
 # total amount of noble cards
@@ -34,7 +35,7 @@ CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
          Card([0, 2, 0, 0, 2], 0, Gem.WHITE),
          Card([0, 3, 0, 0, 0], 0, Gem.WHITE),
          Card([0, 0, 4, 0, 0], 1, Gem.WHITE),
-         
+
          Card([1, 1, 0, 1, 1], 0, Gem.GREEN),
          Card([1, 1, 0, 1, 2], 0, Gem.GREEN),
          Card([0, 1, 0, 2, 2], 0, Gem.GREEN),
@@ -43,7 +44,7 @@ CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
          Card([0, 2, 0, 2, 0], 0, Gem.GREEN),
          Card([0, 0, 0, 3, 0], 0, Gem.GREEN),
          Card([0, 0, 0, 0, 4], 1, Gem.GREEN),
-         
+
          Card([1, 1, 1, 0, 1], 0, Gem.RED),
          Card([2, 1, 1, 0, 1], 0, Gem.RED),
          Card([2, 0, 1, 0, 2], 0, Gem.RED),
@@ -52,14 +53,14 @@ CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
          Card([2, 0, 0, 2, 0], 0, Gem.RED),
          Card([3, 0, 0, 0, 0], 0, Gem.RED),
          Card([4, 0, 0, 0, 0], 1, Gem.RED),
-         
+
          Card([3, 2, 2, 0, 0], 1, Gem.BLACK),
          Card([3, 0, 3, 0, 2], 1, Gem.BLACK),
          Card([0, 1, 4, 2, 0], 2, Gem.BLACK),
          Card([0, 0, 5, 3, 0], 2, Gem.BLACK),
          Card([5, 0, 0, 0, 0], 2, Gem.BLACK),
          Card([0, 0, 0, 0, 6], 3, Gem.BLACK),
-         
+
          Card([0, 2, 2, 3, 0], 1, Gem.BLUE),
          Card([0, 2, 3, 0, 3], 1, Gem.BLUE),
          Card([5, 3, 0, 0, 0], 2, Gem.BLUE),
@@ -87,7 +88,7 @@ CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
          Card([3, 0, 0, 0, 5], 2, Gem.RED),
          Card([0, 0, 0, 0, 5], 2, Gem.RED),
          Card([0, 0, 0, 6, 0], 3, Gem.RED),
-         
+
          Card([3, 3, 5, 3, 0], 3, Gem.BLACK),
          Card([0, 0, 0, 7, 0], 4, Gem.BLACK),
          Card([0, 0, 3, 6, 3], 4, Gem.BLACK),
@@ -111,7 +112,7 @@ CARDS = [Card([1, 1, 1, 1, 0], 0, Gem.BLACK),
          Card([3, 5, 3, 0, 3], 3, Gem.RED),
          Card([0, 0, 7, 0, 0], 4, Gem.RED),
          Card([0, 3, 6, 3, 0], 4, Gem.RED),
-         Card([0, 0, 7, 3, 0], 5, Gem.RED),]
+         Card([0, 0, 7, 3, 0], 5, Gem.RED), ]
 
 NOBLES = [(0, 0, 0, 4, 4),
           (3, 0, 3, 0, 3),
@@ -122,25 +123,48 @@ NOBLES = [(0, 0, 0, 4, 4),
           (0, 3, 3, 3, 0),
           (4, 4, 0, 0, 0),
           (3, 3, 3, 0, 0),
-          (3, 0, 0, 3, 3),]
+          (3, 0, 0, 3, 3), ]
 
-def greedy_play(p:Player, b:Board, p:int, s:int=0):
-    pass
 
-def play(n:int=card_amount, m:int=noble_amount, p:int=15, T:int=5):
-    # initialization
-    cards = CARDS
-    if n < card_amount:
-        cards = random.sample(CARDS, n)
-    
-    nobles = NOBLES
-    if m < noble_amount:
-        nobles = random.sample(NOBLES, m)
-    
-    p = Player()
+def greedy_action(player: Player, board: Board):
+    affordable_cards = [card for card in board.deck if player.can_purchase_card(card)]
+    if affordable_cards:
+        # Prioritize cards by score and then by minimal cost
+        best_card = max(affordable_cards, key=lambda card: (card.score, -sum(card.gems)))
+        player.purchase_card(best_card)
+        board.deck.remove(best_card)
+        print(f"Purchased card with score {best_card.score} and cost {best_card.gems}.")
+    else:
+        # If no affordable card, take gems
+        gems_taken = player.select_gems_to_take(board)
+        print(f"Took gems: {gems_taken}")
 
-    b = Board(cards, nobles, T)
 
-    # play with greedy algo
-    return greedy_play(p, b, p)
-    pass
+board = Board(deck=CARDS, nobles=NOBLES, T=5)  # T=5 assumes 5 tokens of each gem type are available
+
+player = Player()
+
+for turn in range(1, 21):  # Simulate 20 turns
+    print(f"Turn {turn}")
+    greedy_action(player, board)  # Pass the board instance, not the Board class
+    if player.score >= 15:
+        print("Player reached 15 points!")
+        break
+
+# def play(n: int = card_amount, m: int = noble_amount, p: int = 15, T: int = 5):
+#     # initialization
+#     cards = CARDS
+#     if n < card_amount:
+#         cards = random.sample(CARDS, n)
+#
+#     nobles = NOBLES
+#     if m < noble_amount:
+#         nobles = random.sample(NOBLES, m)
+#
+#     p = Player()
+#
+#     b = Board(cards, nobles, T)
+#
+#     # play with greedy algo
+#     return greedy_play(p, b, p)
+#     pass
